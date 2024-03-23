89535bfdc88648bc4651bcb4699882d789a89ec1
Shashank <shm2942004@gmail.com>
2024-03-23 19:00:31 +0530

    further

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("mousemove",function(dets){
     crsr.style.left = dets.x+"px"
     crsr.style.top = dets.y+"px"
     blur.style.left = dets.x - 200 +"px"
-    blur.style.top = dets.y- 200 +"px"
+    blur.style.top = dets.y - 200 +"px"
 })
 
 
